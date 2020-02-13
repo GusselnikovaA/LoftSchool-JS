3268e62d32b64880fefb14b1d7d21af2bf444acb
GusselnikovaA <annanac1993@gmail.com>
2020-02-13 17:57:32 +0300

    Решение 2-4 задачи

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@
    addListener('click', document.querySelector('a'), () => console.log('...')) // должна добавить указанный обработчик кликов на указанный элемент
  */
 function addListener(eventName, target, fn) {
-  target.addEventListener(eventName, fn)
+    target.addEventListener(eventName, fn);
 }
 
 /*
@@ -21,6 +21,7 @@ function addListener(eventName, target, fn) {
    removeListener('click', document.querySelector('a'), someHandler) // должна удалить указанный обработчик кликов на указанный элемент
  */
 function removeListener(eventName, target, fn) {
+    target.removeEventListener(eventName, fn);
 }
 
 /*
@@ -32,6 +33,7 @@ function removeListener(eventName, target, fn) {
    skipDefault('click', document.querySelector('a')) // после вызова функции, клики на указанную ссылку не должны приводить к переходу на другую страницу
  */
 function skipDefault(eventName, target) {
+    target.addEventListener(eventName, (e) => e.preventDefault());
 }
 
 /*
@@ -43,6 +45,7 @@ function skipDefault(eventName, target) {
    emulateClick(document.querySelector('a')) // для указанного элемента должно быть сэмулировано события click
  */
 function emulateClick(target) {
+    target.click();
 }
 
 /*
@@ -55,6 +58,17 @@ function emulateClick(target) {
    delegate(document.body, () => console.log('кликнули на button')) // добавит такой обработчик кликов для body, который будет вызывать указанную функцию только если кликнули на кнопку (элемент с тегом button)
  */
 function delegate(target, fn) {
+    // for (let child of target.children) {
+    //     if (child.tagName == 'button') {
+    //         target.addEventListener('click', fn);
+    //     }
+    // }
+   
+    target.addEventListener('click', function(event) {
+            if (event.target == 'button') {
+                fn();
+            }
+    });
 }
 
 /*
