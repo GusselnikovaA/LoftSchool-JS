815383841a1f15738f4af63ae49e02e32f15c322
Anna <annanac1993@gmail.com>
2020-02-23 12:46:30 +0300

    Вариант решения с наставником

--- src/towns.js
+++ src/towns.js
@@ -37,28 +37,49 @@ const homeworkContainer = document.querySelector('#homework-container');
  https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
  */
 function loadTowns() {
-    let promise = fetch ('https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json')
-        .then(response => {
-            if (response.status >= 400) {
-                return Promise.reject();
-            }
+// МОЕ РЕШЕНИЕ ЧЕРЕЗ FETCH
+//     let promise = fetch ('https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json')
+//         .then(response => {
+//             if (response.status >= 400) {
+//                 return Promise.reject();
+//             }
 
-            return response.json();
-        })
-        .then(towns => {
-            return towns.sort(function (a, b) {
-                if (a.name > b.name) {
-                    return 1;
-                }
-                if (a.name < b.name) {
-                    return -1;
-                }
+//             return response.json();
+//         })
+//         .then(towns => {
+//             return towns.sort(function (a, b) {
+//                 if (a.name > b.name) {
+//                     return 1;
+//                 }
+//                 if (a.name < b.name) {
+//                     return -1;
+//                 }
 
-                return 0;
+//                 return 0;
+//             });
+//         });
+
+//     return promise;
+// }
+
+    return new Promise((resolve, reject) => {
+        fetch ('https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json')
+            .then(response => response.ok ? response.json() : reject())
+            .then (data => {
+                data.sort((a, b) => {
+                    if (a.name < b.name) {
+                        return -1;
+                    }
+                    if (a.name > b.name) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                });
+
+                resolve(data);
             });
-        });
-
-    return promise;
+    })
 }
 
 /*
@@ -72,16 +93,6 @@ function loadTowns() {
    isMatching('Moscow', 'SCO') // true
    isMatching('Moscow', 'Moscov') // false
  */
-function isMatching(full, chunk) {
-    full = full.toLowerCase();
-    chunk = chunk.toLowerCase();
-
-    if (full.indexOf(chunk) >= 0) {
-        return true
-    } else {
-        return false
-    }
-}
 
 /* Блок с надписью "Загрузка" */
 const loadingBlock = homeworkContainer.querySelector('#loading-block');
@@ -91,29 +102,84 @@ const filterBlock = homeworkContainer.querySelector('#filter-block');
 const filterInput = homeworkContainer.querySelector('#filter-input');
 /* Блок с результатами поиска */
 const filterResult = homeworkContainer.querySelector('#filter-result');
+let towns = [];
 
-loadingBlock.style.cssText = "display: none";
-filterBlock.style.cssText = "display: block";
+function isMatching(full, chunk) {
+    // МОЯ ФУНКЦИЯ
+    // full = full.toLowerCase();
+    // chunk = chunk.toLowerCase();
+
+    // if (full.indexOf(chunk) >= 0) {
+    //     return true
+    // } else {
+    //     return false
+    // }
+
+    const reg = new RegExp(chunk, 'i');
+
+    return (full.search(reg) !== -1);
+}
+
+// ЧАСТЬ МОЕГО РЕШЕНИЯ
+// loadingBlock.style.cssText = "display: none";
+// filterBlock.style.cssText = "display: block";
+
+function addTowns(array) {
+    const fragment = document.createDocumentFragment();
+
+    for (const town of array) {
+        if (filterInput.value.length !== 0 && isMatching(town.name, filterInput.value)) {
+            const block = document.createElement ('div');
+
+            block.innerText = town.name;
+            fragment.appendChild(block);
+        }
+    }
+
+    filterResult.prepend(fragment);
+}
+
+function load() {
+    loadTowns()
+        .then(data => {
+            towns = data;
+
+            loadingBlock.style.display = 'none';
+            filterBlock.style.display = 'block';
+        }).catch(() => {
+            loadingBlock.innerText = 'Не удалось загрузить города';
+            const btn = document.createElement('button');
+
+            btn.innerText = 'Повторить';
+            btn.addEventListener('click', load);
+            homeworkContainer.appendChild(btn);
+        })
+}
+
+load();
 
 filterInput.addEventListener('keyup', function() {
-    loadingBlock.style.cssText = "display: block";
+    // МОЕ РЕШЕНИЕ 
+    // loadingBlock.style.cssText = "display: block";
+    // filterResult.innerHTML = '';
+    // loadTowns()
+    //     .then(towns => {
+    //         for (const town of towns) {
+    //             if (isMatching(town.name, filterInput.value) === true) {
+    //                 const div = document.createElement ('div');
+
+    //                 div.textContent = `${town.name}`;
+    //                 filterResult.appendChild(div);
+    //                 loadingBlock.style.cssText = "display: none";
+    //             } 
+
+    //             if (filterInput.value === '') {
+    //                 filterResult.innerHTML = ''
+    //             }
+    //         }
+    //     })
     filterResult.innerHTML = '';
-    loadTowns()
-        .then(towns => {
-            for (const town of towns) {
-                if (isMatching(town.name, filterInput.value) === true) {
-                    const div = document.createElement ('div');
-
-                    div.textContent = `${town.name}`;
-                    filterResult.appendChild(div);
-                    loadingBlock.style.cssText = "display: none";
-                } 
-
-                if (filterInput.value === '') {
-                    filterResult.innerHTML = ''
-                }
-            }
-        })
+    addTowns(towns);
 });
 
 export {
